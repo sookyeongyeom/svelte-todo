c75cc72f13a5fd04e1a8d7f1c8777c01a30dc03a
sookyeongyeom <sookyeong.dev@gmail.com>
2022-07-03 00:54:54 +0900

    :sparkles: done

---
+++ src/components/TodoList.svelte
@@ -0,0 +1,16 @@
+<script>
+	import TodoItem from './TodoItem.svelte';
+
+    export let todos;
+    export let handleCheckTodo;
+</script>
+
+<div class=main>
+    <ul>
+        {#each todos as todo, index(todo)}
+        <li>
+            <TodoItem {todo} {handleCheckTodo}/>
+        </li>
+        {/each}
+    </ul>
+</div>
